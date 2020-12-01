15672bfdfc32655098a85bc9352bad8904458e90
stellastak <stellastak@hotmail.com>
2020-12-01 22:37:13 +0100

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,19 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
+sys.path.append(os.path.abspath('..'))
 
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
-project = 'The Extended SG'
-copyright = '2020, Stella Stakiadi'
-author = 'Stella Stakiadi'
+project = 'The Extended Selfish Gene'
+copyright = '2020, Giovanni Squillero'
+author = 'Giovanni Squillero'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -38,8 +40,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'pages/reference-architecture', 'some/other/file.txt']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -51,4 +52,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
