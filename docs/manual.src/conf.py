e9cf42c6214d05baf30ef326b160ca53ad98bbcb
stellastak <stellastak@hotmail.com>
2020-12-17 22:03:42 +0100

    logo

--- docs/manual.src/conf.py
+++ docs/manual.src/conf.py
@@ -65,7 +65,7 @@ html_show_sphinx = True
 html_theme = 'sphinx_rtd_theme'
 
 html_show_sourcelink = False
-html_logo = '../images/logo/dna_tran.png'
+html_logo = '../images/logo/trans_dna.jpg'
 html_favicon = '../images/icons/favicon.png'
 
 
